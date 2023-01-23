731a2993c1acf9f6574276473686167041930e35
Priya Misner <priyamisner@gmail.com>
2023-01-23 11:36:37 -0500

    data section blocked out

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,38 @@ import styled from 'styled-components';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import ExploreSections from './components/ExploreSections';
+import {
+  CountryCompareSection,
+  PresentWorldSection,
+  PastDecadeSection,
+} from './components/Minicharts';
 import { maxWidth } from './styleConstants';
 
 const AllContent = styled.h1`
   margin: 0 auto;
   max-width: ${maxWidth};
 `;
+function sectionReturn(item) {
+  switch (item) {
+    case 'presentWorld':
+      return <PresentWorldSection />;
+    case 'pastDecade':
+      return <PastDecadeSection />;
+    case 'countryCompare':
+      return <CountryCompareSection />;
+    default:
+      return <p>Something went wrong</p>;
+  }
+}
 
 function App() {
-  const [theme, setTheme] = useState('presentWorld'); // options: presentWorld, pastDecade, countryCompare
-
+  const [theme, setTheme] = useState('presentWorld');
   return (
     <AllContent>
       <Header />
       <Hero />
       <ExploreSections theme={theme} setTheme={setTheme} />
+      {sectionReturn(theme)}
     </AllContent>
   );
 }
